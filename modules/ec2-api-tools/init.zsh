6574fd845ce11f98ecc1047680fe87c609ab94a7
Lance Speelmon <lance@speelmon.com>
2013-11-27 12:01:53 -0700

    Updated ec2 tools home dir path for latest keg

--- modules/ec2-api-tools/init.zsh
+++ modules/ec2-api-tools/init.zsh
@@ -27,7 +27,7 @@ fi
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.6.12.0/libexec"
 
 #
 # Aliases
